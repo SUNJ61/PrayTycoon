e66f11e51b6966f2c26a8f9741938b66161b30e0
SunJ <sunhiro0601@naver.com>
2025-09-16 15:36:48 +0900

    Making Sub Map

--- Assets/2.Script/Common/Singleton.cs
+++ Assets/2.Script/Common/Singleton.cs
@@ -13,7 +13,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
             {
                 instance = FindObjectOfType<T>(); //T 타입의 오브젝트를 찾아 인스턴스로 할당. (T는 각 매니저 클래스로 변경이 됨.)
 
-                if (instance == null) //없으면 미리 씬에 매니저가 생성 되지 않았음을 알림.
+                if (instance == null) //없으면 미리 씬에 매니저가 생성 되지 않았음을 알림. (추후 삭제)
                 {
                     Debug.LogError($"{typeof(T).Name} 인스턴스를 찾을 수 없습니다.");
                 }
@@ -32,6 +32,6 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
         instance = this as T; //기존 매니저가 없다면, T 타입에 해당하는 싱글톤 생성.
 
-        //DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 }
