827a1ebcd83ff2380a528c6aefb18a781cd73bad
SunJ <sunhiro0601@naver.com>
2025-09-25 19:27:07 +0900

    Add MapData

---
+++ Assets/2.Script/Data/MapObjectData.cs
@@ -0,0 +1,13 @@
+[System.Serializable]
+public class MapObjectData
+{
+    public string objectId;   // 고유 ID
+    public bool isRepaired;   // 수리 여부
+    public float posX, posY, posZ; // 위치
+}
+
+[System.Serializable]
+public class MapSaveData
+{
+    public List<MapObjectData> objects = new();
+}
