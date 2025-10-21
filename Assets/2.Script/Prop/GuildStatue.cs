351764d2ee647319c7c467d8d9ad2f528c4facdd
SunJ <sunhiro0601@naver.com>
2025-10-21 18:15:52 +0900

    Update Guild Map

---
+++ Assets/2.Script/Prop/GuildStatue.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuildStatue : MonoBehaviour
+{
+    void Start() //길드 등록 스크립트, 필요기능 소환석 등록, 자동 파밍 기능 활성화, 
+    {
+
+    }
+
+    private void OnTigerEnter2D(Collider2D col)
+    {
+        //UI 띄우기 (길드 UI 새로 만들어야함.)
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        
+    }
+}
