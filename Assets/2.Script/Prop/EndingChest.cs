6f81f79d5bde06f30ce1d1e62bb94dc72d53a6e9
SunJ <sunhiro0601@naver.com>
2025-11-20 21:43:51 +0900

    엔딩 시스템 제작 진행 중.

--- Assets/2.Script/Prop/EndingChest.cs
+++ Assets/2.Script/Prop/EndingChest.cs
@@ -2,16 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndingChest : MonoBehaviour
+public class EndingChest : MonoBehaviour, IQuest
 {
-    // Start is called before the first frame update
-    void Start()
+    public int QuestID { get; private set; }
+    public bool IQuestClear => QuestClear;
+
+    private int FixCredit = 5;
+    private int EndingCredit = 5;
+
+    private string FixKey = "Chest";
+    private string EndingKey = "Ending";
+    private string FixCreditType = "Stone";
+    private string EndingCreditType = "Gold";
+
+    private bool QuestClear = false;
+    private void Start()
     {
-        
+        QuestID = 4;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTiggerEnter2D(Collider2D collision)
+    {
+        if (QuestClear == false) //수리가 되기 전 사용함수.
+        {
+            ButtonManager.Instance.SetCurrentQuest(QuestID);
+            ButtonManager.Instance.ButtonUpdate(0);
+
+            UIManager.Instance.QuestUIEdit(FixKey);
+            QuestManager.Instance.QuestCheck(FixKey, FixCreditType, FixCredit, this);
+
+            UIManager.Instance.GuideUIControl(true);
+        }
+        else //수리가 된 후 사용함수.
+        {
+            //ButtonManager.Instance.SetCurrentSummon(SummonID);
+            //ButtonManager.Instance.ButtonUpdate(1);
+
+            //UIManager.Instance.SummonUIEdit(SpawnKey);
+            //QuestManager.Instance.QuestCheck(SpawnKey, SpawnCreditType, SpawnCredit, this);
+
+            //UIManager.Instance.GuideUIControl(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ButtonManager.Instance.ButtonClear();
+        UIManager.Instance.GuideUIControl(false);
+    }
+
+    public void SetQuestClear() // 퀘스트가 성공하면 발생하는 이벤트.
     {
         
     }
