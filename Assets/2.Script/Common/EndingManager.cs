1dfe15247b4929902f970c6cc158b95cff7bab81
SunJ <sunhiro0601@naver.com>
2026-03-28 19:27:48 +0900

    엔딩 제작 완료 및 씬 연결 완료.

--- Assets/2.Script/Common/EndingManager.cs
+++ Assets/2.Script/Common/EndingManager.cs
@@ -1,15 +1,29 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public SpriteRenderer GemChest;
+    public GameObject EndingUI;
 
-    private float MaxAlpha = 255;
+    public TextMeshProUGUI[] EndingTexts;
+    public Image LobbyBTNImg;
+    public TextMeshProUGUI LobbyBTNText;
+
+    private Button LobbyBTN;
+    private float MaxAlpha = 1.0f;
+    private float DelayBetweenText = 0.5f;
+
+    WaitForSeconds delay = new WaitForSeconds(0.01f);
 
     private void Awake()
     {
+        LobbyBTN = LobbyBTNImg.gameObject.GetComponent<Button>();
+        LobbyBTN.onClick.AddListener(()=>SceneLoadManager.Instance.ExitGame("TycoonLobby"));
+
         StartCoroutine(EndingActive());
     }
 
@@ -24,14 +38,59 @@ public class EndingManager : MonoBehaviour
             gemchest.a = alpha;
             GemChest.color = gemchest;
 
-            yield return null;
+            yield return delay;
 
-            if(GemChest.color.a >= MaxAlpha)
+            if(alpha >= MaxAlpha)
             {
-                //엔딩 멘트 UI 등장 코드 추가 필요
+                LobbyBTN.interactable = false;
+                EndingUI.SetActive(true);
+                yield return StartCoroutine(EndingUIUpdate());
                 break;   
             }
         }
     }
 
+    private IEnumerator EndingUIUpdate()
+    {
+        for (int i = 0; i < EndingTexts.Length; i++)
+        {
+            yield return StartCoroutine(FadeInText(EndingTexts[i]));
+            yield return new WaitForSeconds(DelayBetweenText);
+        }
+
+        yield return StartCoroutine(FadeInBTN());
+    }
+
+    private IEnumerator FadeInText(TextMeshProUGUI text)
+    {
+        Color textAlpha = text.color;
+        float alpha = 0;
+
+        while(alpha <= MaxAlpha)
+        {
+            alpha += Time.deltaTime;
+            textAlpha.a = alpha;
+            text.color = textAlpha;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeInBTN()
+    {
+        Color textAlpha = LobbyBTNImg.color;
+        float alpha = 0;
+
+         while(alpha <= MaxAlpha)
+        {
+            alpha += Time.deltaTime;
+            textAlpha.a = alpha;
+            LobbyBTNImg.color = textAlpha;
+            LobbyBTNText.color = textAlpha;
+
+            yield return null;
+        }
+
+        LobbyBTN.interactable = true;
+    }
 }
