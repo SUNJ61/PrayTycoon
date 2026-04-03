22fef8f6cca778bcdd9637ea9cb8075eca3a98ad
SunJ <sunhiro0601@naver.com>
2026-04-03 22:04:17 +0900

    오디오  로직 제작

--- Assets/2.Script/Common/SoundManager.cs
+++ Assets/2.Script/Common/SoundManager.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    public AudioMixer audioMixer;
+
+    public void PlayBGM(AudioClip clip, bool loop = true) //BGM 재생
     {
-        
+        if (bgmSource.clip == clip) return; 
+
+        bgmSource.clip = clip;
+        bgmSource.loop = loop;
+        bgmSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySFX(AudioClip clip) // 위치 지정이 필요없는 SFX 소리 재생
     {
-        
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PlaySFX(AudioClip clip, Vector3 position) //위치 지정이 필요한 SFX 소리 재생
+    {
+        AudioSource.PlayClipAtPoint(clip, position);
+    }
+
+    public void SetVolume(string name, float sliderValue) //옵션으로 소리 조절시 소리값 조절
+    {
+        audioMixer.SetFloat(name, Mathf.Log10(sliderValue) * 20);
     }
 }
