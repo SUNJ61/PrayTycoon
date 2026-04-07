4d40063deb327e0e6e7ac752356e663f8e4cfa61
SunJ <sunhiro0601@naver.com>
2026-04-07 23:17:15 +0900

    게임 사운드 데이터 추가 및 게임 사운드 관리 로직 제작 중

--- Assets/2.Script/Common/SoundManager.cs
+++ Assets/2.Script/Common/SoundManager.cs
@@ -1,30 +1,61 @@
 using UnityEngine.Audio;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class SoundManager : Singleton<SoundManager>
 {
-    public AudioSource bgmSource;
-    public AudioSource sfxSource;
+    private List<AudioSource> SoundBoxPool = new List<AudioSource>();
+    private List<AudioSource> LoopSoundBox = new List<AudioSource>();
+    private GameObject SoundBoxPrefab;
+    private int PoolCount = 10;
+
+    public List<SoundData> GameSound;
 
     public AudioMixer audioMixer;
+    public AudioMixerGroup BGM;
+    public AudioMixerGroup SFX;
 
-    public void PlayBGM(AudioClip clip, bool loop = true) //BGM 재생
+    protected override void OnAwake()
     {
-        if (bgmSource.clip == clip) return; 
+        // 사운드 박스 프리팹 리소스에서 가져오기.
 
-        bgmSource.clip = clip;
-        bgmSource.loop = loop;
-        bgmSource.Play();
+        SoundBoxPooling(); //사운드 박스 오브젝트 풀링
     }
-
-    public void PlaySFX(AudioClip clip) // 위치 지정이 필요없는 SFX 소리 재생
+    public void PlaySound(string name) // 게임 배경음 재생
     {
-        sfxSource.PlayOneShot(clip);
-    }
+        SoundData data = GameSound.Find(s => s.ClipName == name);
 
-    public void PlaySFX(AudioClip clip, Vector3 position) //위치 지정이 필요한 SFX 소리 재생
-    {
-        AudioSource.PlayClipAtPoint(clip, position);
+        if(data != null)
+        {
+            if(data.clip == null) return;
+            //사운드 박스 소환, 사운드 플레이 코드 필요 
+            AudioSource source = SoundBoxPool.Find(s => !s.gameObject.activeSelf); //폴링된 리스트에서 비활성화 된 사운드 박스 찾기.
+
+            if(source == null) source = CreateNewSoundBox(); //사운드 박스가 부족할 시 추가 생성.
+            
+            source.gameObject.SetActive(true);
+            
+            switch(data.SoundType) //사운드 박스 믹서 그룹 할당.
+            {
+                case "BGM":
+                source.outputAudioMixerGroup = BGM;
+                    break;
+
+                case "SFX":
+                source.outputAudioMixerGroup = SFX;
+                    break;
+            }
+
+            source.clip = data.clip;
+            source.loop = data.loop;
+            source.Play();
+
+            if(data.loop == false)
+                StartCoroutine(EndSFX(source));
+            else
+                LoopSoundBox.Add(source);
+        }
     }
 
     public void SetVolume(string name, float sliderValue) //옵션으로 소리 조절시 소리값 조절
@@ -33,5 +64,36 @@ public class SoundManager : Singleton<SoundManager>
             audioMixer.SetFloat(name, -80f);
         else
             audioMixer.SetFloat(name, Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void EndBGM() //루프로 돌고있는 BGM 끄기
+    {
+        //소스에 할당된 BGM, SFX초기화 필요, 할당된 클립 제거 필요, 사운드 박스 비활성화 필요
+    }
+
+    private void SoundBoxPooling()
+    {
+        for(int i = 0; i < PoolCount; i++)
+        {
+            CreateNewSoundBox();
+        }
+    }
+
+    private AudioSource CreateNewSoundBox()
+    {
+        GameObject soundbox = Instantiate(SoundBoxPrefab, transform);
+        AudioSource audioSource = soundbox.GetComponent<AudioSource>();
+        soundbox.SetActive(false);
+        SoundBoxPool.Add(audioSource);
+
+        return audioSource;
+    }
+
+    private IEnumerator EndSFX(AudioSource source)
+    {
+        yield return new WaitForSeconds(source.clip.length);
+        source.outputAudioMixerGroup = null;
+        source.clip = null;
+        source.gameObject.SetActive(false);
     }
 }
--- Assets/2.Script/Data/SoundData.cs
+++ Assets/2.Script/Data/SoundData.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundData : MonoBehaviour
+[CreateAssetMenu(fileName = "NewSoundData", menuName = "Sound/SoundData")]
+public class SoundData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string SoundType;
+    public string ClipName;
+    public AudioClip clip;
+    [Range(0, 1)] public float Voulume = 0.5f;
+    public bool loop;
 }
